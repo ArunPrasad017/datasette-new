a046bf2df5db38b6f4a217c3d1b77995d60ebf90
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-04-04 13:57:39 -0700

    Added file for elbs creation

---
+++ infra/datasette_elbs.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_elastic_beanstalk_application" "datasette_docker_app" {
+  name        = "datasette-docker-app"
+  description = "placeholder datasette-docker-app for deploying datasette image to elbs environment"
+}
+
+resource "aws_elastic_beanstalk_environment" "datasette_docker_env" {
+  name                = "datasette-docker-env"
+  application         = aws_elastic_beanstalk_application.datasette_docker_app.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.2.6 running Docker"
+  tier                = "WebServer"
+}
