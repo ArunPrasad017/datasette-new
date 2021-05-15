e1d4843f548c4c791766a4b0a2bbbdb6c8a6cd27
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-05-15 15:53:39 -0700

    Added the new bucket used in latest aws acct

--- infra/test_s3.tf
+++ infra/test_s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "bucket1"{
 
 terraform {
   backend "s3" {
-    bucket  = "aruntestbucketaws"
+    bucket  = "arun-useast1-dev-infra"
     key     = "state/s3/s3_test.tfstate"
     region  = "us-east-1"
     encrypt = true
