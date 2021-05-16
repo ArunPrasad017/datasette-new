0121bf32f432032ac627a84cee2ae679131e3b0b
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-05-15 17:50:36 -0700

    change to env

--- infra/datasette_elbs.tf
+++ infra/datasette_elbs.tf
@@ -6,7 +6,7 @@ resource "aws_elastic_beanstalk_application" "datasette_docker_app" {
 resource "aws_elastic_beanstalk_environment" "datasette_docker_env" {
   name                = "datasette-docker-env"
   application         = aws_elastic_beanstalk_application.datasette_docker_app.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.2.6 running Docker"
+  solution_stack_name = "64bit Amazon Linux 2 v3.3.0 running Docker"
   tier                = "WebServer"
 
   setting {
