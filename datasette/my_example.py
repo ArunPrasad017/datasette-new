9232f80a068a4266d2e7b4c98458f148b33d116b
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-06-05 20:40:21 -0700

    Adding agg query to run

--- datasette/my_example.py
+++ datasette/my_example.py
@@ -12,8 +12,6 @@ url = "https://datahub.io/sports-data/spanish-la-liga/r/season-1819.json"
 
 
 def initialize_db(timestamp: datetime) -> datetime:
-    print(f"Inside the docker file at - {timestamp}")
-    log.debug("This is a debugging message")
     db = sqlite_utils.Database("laliga1819.db")
     db["laliga1819"].insert_all(
         requests.get(url).json(),
@@ -22,9 +20,17 @@ def initialize_db(timestamp: datetime) -> datetime:
     return timestamp
 
 
+def run_aggregate_query() -> None:
+    with open("agg_query.sql", "r") as fd:
+        sqlFile = fd.read()
+    db = sqlite_utils.Database(sqlite_utils.connect("my_database.db"))
+    db.execute(sqlFile)
+
+
 def main() -> None:
     now = datetime.now()
     initialize_db(now)
+    run_aggregate_query()
 
 
 if __name__ == "__main__":
