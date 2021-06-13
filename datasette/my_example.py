72cc9997c960209e0e76f1798d4b116e6c659189
arunprasad017 <21147125+ArunPrasad017@users.noreply.github.com>
2021-06-13 11:36:13 -0700

    Added a) Try catch for db b) Try catch for requests path c) stopping deployment booleans d) Remove datetime

--- datasette/my_example.py
+++ datasette/my_example.py
@@ -1,7 +1,6 @@
 """
 """
 import logging
-from datetime import datetime
 
 import requests
 import sqlite_utils
@@ -11,27 +10,34 @@ log = logging.getLogger(__name__)
 url = "https://datahub.io/sports-data/spanish-la-liga/r/season-1819.json"
 
 
-def initialize_db(timestamp: datetime) -> datetime:
+def initialize_db() -> None:
     db = sqlite_utils.Database("laliga1819.db")
-    db["laliga1819"].insert_all(
-        requests.get(url).json(),
-        pk="id",
-    )
-    return timestamp
+    try:
+        db["laliga1819"].insert_all(
+            requests.get(url).json(),
+            pk="id",
+        )
+    except requests.ConnectionError:
+        log.error("URL undefined")
+        return None
+    return
 
 
 def run_aggregate_query() -> None:
     log.info("Read SQL file")
     with open("agg_query.sql", "r") as fd:
         sqlFile = fd.read()
-    db = sqlite_utils.Database("laliga1819.db")
+    try:
+        db = sqlite_utils.Database("laliga1819.db")
+    except Exception as E:
+        log.error("DB not found - Exception: {}".format(E))
+        return
     log.info("Executing SQL file on laliga db")
     db.execute(sqlFile)
 
 
 def main() -> None:
-    now = datetime.now()
-    initialize_db(now)
+    initialize_db()
     run_aggregate_query()
 
 
